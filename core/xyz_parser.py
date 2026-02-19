84bf0942a4fa6151e1ace1ed8f55b9b579570884
rkrsinu <152066159+rkrsinu@users.noreply.github.com>
2026-02-19 20:39:24 +0530

    Update xyz_parser.py

--- core/xyz_parser.py
+++ core/xyz_parser.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def parse_xyz(file):
+
     lines = file.read().decode().splitlines()[2:]
 
-    elements, coords = [], []
+    elements = []
+    coords = []
 
     for line in lines:
         e, x, y, z = line.split()
