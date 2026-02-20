c3044ab14f98bd946986a6124ef63c600fceb49b
rkrsinu <152066159+rkrsinu@users.noreply.github.com>
2026-02-20 20:18:43 +0530

    Update predictor.py

--- core/predictor.py
+++ core/predictor.py
@@ -1,15 +1,7 @@
+from pathlib import Path
 import joblib
-import numpy as np
 
-# load once (fast for Streamlit)
-model_D  = joblib.load("model/RF_model_D.joblib")
-model_ED = joblib.load("model/RF_model_ED.joblib")
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-def predict(features: np.ndarray):
-
-    features = features.reshape(1, -1)
-
-    D  = model_D.predict(features)[0]
-    ED = model_ED.predict(features)[0]
-
-    return D, ED
+model_D  = joblib.load(BASE_DIR / "model/RF_model_D.joblib")
+model_ED = joblib.load(BASE_DIR / "model/RF_model_ED.joblib")
